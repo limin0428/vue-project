4edae464cfaa0d45b61da9dca7cdb2b96942fa0a
limin0428 <1690324671@qq.com>
2023-04-08 15:23:02 +0800

    feat: 增加常用算法函数

--- algorithm/index.js
+++ algorithm/index.js
@@ -3,10 +3,10 @@
  * @param { string, boolean } 
  * @return { boolean }
  */
-const isPalindrome = (str = '', isCaseSensitive = false) => {
+export const isPalindrome = (str = '', isCaseSensitive = false) => {
     if (!str) {
         return true;
-    };
+    }
     let left = 0;
     let right = str.length - 1;
     let isEq = true;
@@ -16,9 +16,9 @@ const isPalindrome = (str = '', isCaseSensitive = false) => {
         isEq = isCaseSensitive ? str[left] === str[right] : str[left].toLowerCase() === str[right].toLowerCase();
         if (!isEq) {
             return false
-        };
+        }
         left++; right--
-    };
+    }
     return true
 };
 
@@ -27,17 +27,17 @@ const isPalindrome = (str = '', isCaseSensitive = false) => {
  * @param { string } digits 数字按键 eg: '23'
  * @return { string[] }
  */
-const keyboardMap = (digits) => {
+export const keyboardMap = (digits) => {
     let res = [];
     const _compose = (arr1 = [], arr2 = []) => {
         let r = [];
-        if (arr1.length === 0) { return arr2 };
-        if (arr2.length === 0) { return arr1 };
+        if (arr1.length === 0) { return arr2 }
+        if (arr2.length === 0) { return arr1 }
         for (let i = 0; i < arr1.length; i++) {
             for (let j = 0; j < arr2.length; j++) {
                 r.push(`${arr1[i]}${arr2[j]}`)
             }
-        };
+        }
         return r;
     }
     const map = ['', '', 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz'];
@@ -53,12 +53,12 @@ const keyboardMap = (digits) => {
  * @param { number } maxNum
  * @return { any[] }
 */ 
-const concurRequest = (urls, maxNum) => {
+export const concurRequest = (urls, maxNum) => {
     return new Promise(reslove => {
         if (urls.length === 0) {
             reslove([]);
             return
-        };
+        }
         const result = [];
         let index = 0;
         let count = 0;
@@ -97,7 +97,7 @@ const concurRequest = (urls, maxNum) => {
  * 等差数列求和
  * @param a1:首项 d:公差 n:项数
 */ 
-const ArithmeticProgressionSum = (a1, d, n) => {
+export const ArithmeticProgressionSum = (a1, d, n) => {
     return a1 * n + (n * (n - 1) * d) / 2
 }
 
@@ -105,7 +105,7 @@ const ArithmeticProgressionSum = (a1, d, n) => {
  * 等比数列求和
  * @param a1:首项 q:公比 n:项数
 */ 
-const GeometricProgressionSum = (a1, q, n) => {
+export const GeometricProgressionSum = (a1, q, n) => {
     return a1 * (1 - Math.pow(q, n)) / (1 - q)
 }
 
@@ -114,13 +114,78 @@ const GeometricProgressionSum = (a1, q, n) => {
  * 判断是否为素数
  * @param num:数字
 */
-const isPrimeNumber = (num) => {
-    if (num > 4) { return false };
+export const isPrimeNumber = (num) => {
+    if (num > 4) { return false }
     for (let i = 2; i < Math.sqrt(num); i++) {
         if(num%i === 0){
-            return false
-        };
-    };
+            return false;
+        }
+    }
     return true;
 }
+
+/**
+ * 统计字符串每个字符出现的次数
+ * @param str:字符串
+ */ 
+export const characterCount = (str) => {
+    if (typeof str !== 'string') {
+        throw new TypeError('str is not string!')
+    }
+    return str.split('').reduce((pre, next) => {
+        pre[next] = pre[next] ? pre[next] + 1 : 1;
+        return pre;
+    }, {})
+}
+
+/**
+ * 
+ * 
+*/
+export const request = (url, maxCount = 1) => {
+    return fetch(url).catch(err => maxCount <= 0 ? Promise.reject(err) : request(url, maxCount - 1));
+}
+
+/***
+ * 动态属性
+ * add[1][2][3] + 4  // 10
+ */
+export const add = new Proxy({
+    _store: 0
+}, {
+    get(target, key, rexeiver) {
+        if (key === Symbol.toPrimitive) {
+            return () => target._store
+        }
+        target._store += +key;
+        return rexeiver
+    }
+})
+
+/**
+ * 字符串截取 码元和码点 
+ * 码元: 16位  码点:16位/32位   
+*/
+String.prototype.sliceByPoint = function (start, end) {
+    let result = '';
+    let pIndex = 0;
+    let cIndex = 0;
+    while(true){
+        if (pIndex >= end || cIndex >= this.length) {
+            break;
+        }
+        // 获取该字符的码元值 
+        const point = this.codePointAt(cIndex);
+        if (pIndex >= start) {
+            result+= String.fromCodePoint(point);
+        }
+        cIndex+= point > 0xffff ? 2 : 1;
+        pIndex++;
+    }
+    return result;
+};
+
+
+
+
 
--- src/components/AnnularLayout.vue
+++ src/components/AnnularLayout.vue
@@ -1,82 +1,84 @@
 <template>
- <div class="container" :style="style">
-    <div class="item" 
-        v-for="(item, index) in props.imgList" :key="index" 
-        :style="{ transform: `translate(${getPos(index).x}px, ${getPos(index).y}px)` }"
+  <div class="container" :style="style">
+    <div
+      class="item"
+      v-for="(item, index) in props.imgList"
+      :key="index"
+      :style="{ transform: `translate(${getPos(index).x}px, ${getPos(index).y}px)` }"
     >
-        <slot :name="`item${index}`">
-            <img :src="item" alt="" >
-        </slot>
+      <slot :name="`item${index}`">
+        <img :src="item" alt="" />
+      </slot>
     </div>
- </div>
+  </div>
 </template>
 
 <script setup>
- import { defineProps, computed } from 'vue';
- const props = defineProps({
-    imgList: {
-        type: Array,
-        default: () => []
-    },
-    width: {
-        type: String,
-        default: '400px'
-    },
-    height: {
-        type: String,
-        default: '400px'
-    },
-    itemWidth: {
-        type: String,
-        default: '90px'
-    },
-    itemHeight: {
-        type: String,
-        default: '90px'
-    },
- });
- const style = computed(() => {
-    return {
-        '--width': props.width,
-        '--height': props.height,
-        '--itemWidth': props.itemWidth,
-        '--itemHeight': props.itemHeight,
-    }
- })
+import { defineProps, computed } from 'vue';
+const props = defineProps({
+  imgList: {
+    type: Array,
+    default: () => [],
+  },
+  width: {
+    type: String,
+    default: '400px',
+  },
+  height: {
+    type: String,
+    default: '400px',
+  },
+  itemWidth: {
+    type: String,
+    default: '90px',
+  },
+  itemHeight: {
+    type: String,
+    default: '90px',
+  },
+});
+const style = computed(() => {
+  return {
+    '--width': props.width,
+    '--height': props.height,
+    '--itemWidth': props.itemWidth,
+    '--itemHeight': props.itemHeight,
+  };
+});
 
- const getPos = (index) => {
-    const r = parseInt(props.width) / 2;
-    const pieceDeg = 360 / props.imgList.length;
-    const t = (Math.PI / 180) * index * pieceDeg;
-    const x = Math.sin(t) * r - parseInt(props.itemWidth) / 2;
-    const y = -Math.cos(t) * r - parseInt(props.itemHeight) / 2;
-    return { x, y }
- }
+const getPos = (index) => {
+  const r = parseInt(props.width) / 2;
+  const pieceDeg = 360 / props.imgList.length;
+  const t = (Math.PI / 180) * index * pieceDeg;
+  const x = Math.sin(t) * r - parseInt(props.itemWidth) / 2;
+  const y = -Math.cos(t) * r - parseInt(props.itemHeight) / 2;
+  return { x, y };
+};
 </script>
 
 <style lang="less" scoped>
-.container{
-        margin: 16px auto;
-        width: var(--width);
-        height: var(--height);
-        border-radius: 50%;
-        border: 2px solid;
-        position: relative;
-        animation: rotate linear infinite 20s;
-    }
-    .item {
-        width: var(--itemWidth);
-        height: var(--itemHeight);
-        border-radius: 50%;
-        border-radius: 50%;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        overflow: hidden;
-    }
-    .item img {
-        width: 100%;
-        height: 100%;
-        animation: rotate linear infinite 20s reverse;
-    }
+.container {
+  margin: 16px auto;
+  width: var(--width);
+  height: var(--height);
+  border-radius: 50%;
+  border: 2px solid;
+  position: relative;
+  animation: rotate linear infinite 20s;
+}
+.item {
+  width: var(--itemWidth);
+  height: var(--itemHeight);
+  border-radius: 50%;
+  border-radius: 50%;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  overflow: hidden;
+}
+.item img {
+  width: 100%;
+  height: 100%;
+  animation: rotate linear infinite 20s reverse;
+}
 </style>
